6cbd8f160f987426dc38dcfc567d45b1756e39b8
yanastany <anastan0205@gmail.com>
2022-06-01 12:08:33 +0300

    Add players

--- proiect/src/components/Players.jsx
+++ proiect/src/components/Players.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Create_Players from './crud_players/create_players';
+import Add_players from "./crud_players/add_players";
 import Read_Players from './crud_players/read_players';
 import Update_Players from './crud_players/update_players';
 import './Players.css';
@@ -7,7 +8,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 function Players() {
     return (
       <div className="main">
+
+        
+<div className="pls">
+           <Add_players/> 
+        
+      </div>
         <h2 className="main-header">Players List</h2>
+
        
         <div className="pls">
            <Read_Players/> 
